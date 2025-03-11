54af1edde3e909a2487c3f18e1808758216d27cf
miguelricardo <altaiba3@gmail.com>
2025-03-10 19:48:28 -0600

    update main

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ async def root():
 #Define a route that GET requests at the root URL /
 @app.get("/data") 
 async def det_users():
-    async with httpx.AsyncClient() as client:
-        response = await client.get("https://jsonplaceholder.typicode.com/todos/1")
-        return response.json()
+    url = "https://jsonplaceholder.typicode.com/posts/1/comments"
+    try:
+        async with httpx.AsyncClient(timeout=10) as client:
+            response = await client.get(url)
+            response.raise_for_status() 
+            return response.json()
+    except httpx.HTTPStatusError as e:
+        print("f Error on response {e.response.status_code} - {e.response.text}")
+        return {"error": "f HTTP error {e.response.status_code}" }
+    except httpx.RequestError as e:
+        print(f" Error on request {str(e)}")
+        return {"error" : "request error"}
 
